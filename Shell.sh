903aecb69fe5203d31051632ec6591a62a4f34ec
HwangHsing <1254105105@qq.com>
2021-05-31 19:50:20 +0800

    first commit

--- Shell.sh
+++ Shell.sh
@@ -7,8 +7,10 @@ elif which yum >/dev/null;then
 	echo "yum"
 	sudo yum install -y gcc vim git ctags xclip python-setuptools python-devel zsh cscope 
 	git clone git://github.com/joelthelion/autojump.git
-    sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-    sudo yum install -y ripgrep
+	sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+    	sleep 1
+	sudo yum install -y ripgrep
+    sudo yum install wget git -y
 fi
 
 if which brew >/dev/null;then
@@ -18,20 +20,18 @@ fi
 #cscope 安装
 sudo easy_install -ZU autopep8 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
-yum install zsh -y
-chsh -s /bin/zsh root
-echo $SHELL
-yum install wget git -y
 # oh-my-zsh 安装
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sleep 1
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#chsh -s /bin/zsh 
 /bin/cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 #安装autosuggestion
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-sleep 1
+cd ~/.oh-my-zsh/plugins
+git clone git://github.com/zsh-users/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-syntax-highlighting
+sed -i  '30 i plugins = (git autojump zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
 #安装 highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sleep 1
+
 source ~/.zshrc
 
 #sudo chsh -s /bin/zsh
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-git clone --recursive --depth=1 https://github.com/HwangHsing/backplus2/.git ~/vim-for-coding
 cd ~
+git clone https://github.com/HwangHsing/backplus2.git
+#git clone --recursive --depth=1 https://github.com/HwangHsing/backplus2/.git ~/backplus2
 mv .vim .vimbak &> /dev/null
 mv .vimrc .vimrcbak &> /dev/null
-mv vim-for-coding .vim
+mv backplus2 .vim
 ln -s .vim/vimrc .vimrc
 
 #升级vim到8.2否则部分功能不可用
@@ -16,6 +16,7 @@ cd ~
 git clone https://github.com/vim/vim.git
 sleep 1
 cd vim
+#安装python3
 ./configure --prefix=/usr/local/vim  --enable-pythoninterp=yes --enable-python3interp=yes --with-python-command=python --with-python3-command=python36
 sleep 1
 sudo make
@@ -24,8 +25,7 @@ sudo make install
 sudo cp /usr/bin/vim /usr/bin/vim.backup
 cd ~/vim/src
 sudo cp vim /usr/bin
-echo "PATH=\$PATH:/usr/local/vim/bin" >> /etc/profile
-source /etc/profile
+sudo echo "PATH=\$PATH:/usr/local/vim/bin" >> /etc/profile
 
 echo 'success! enjoy it!'
 exit 0
